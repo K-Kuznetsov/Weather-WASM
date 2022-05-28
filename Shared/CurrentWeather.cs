c4d0f8a95f0061ff15ebeb9646c496a31f6d70e3
Kimmo Kuznetsov <Kimmo.Kuznetsov@tptlive.ee>
2022-05-28 23:51:48 +0300

    Updated Index.razor layout

---
+++ Shared/CurrentWeather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weather.Shared
+{
+    public class CurrentWeather
+    {
+        public double CurrTemp { get; set; }
+        public double CurrWind { get; set; }
+        public double FeelsLike { get; set; }
+        public string CurrSummary { get; set; }
+        public string CurrICON { get; set; }
+    }
+}
